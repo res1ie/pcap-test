b00f2b2f761365fe172c0c5a505bf33bbd7f3cfa
res1ie <bchan052415@gmail.com>
2023-03-22 17:01:01 +0900

    fin

--- pcap-test.c
+++ pcap-test.c
@@ -45,8 +45,25 @@ int main(int argc, char* argv[]) {
 			break;
 		}
 		printf("%u bytes captured\n", header->caplen);
+        if(*(uint16_t*)(packet+0xc)!=0x8)continue;
+        if(packet[0x17]!=0x6)continue;
+        printf("src mac %02x:%02x:%02x:%02x:%02x:%02x\n",packet[6],packet[7],packet[8],packet[9],packet[10],packet[11]);
+        printf("dst mac %02x:%02x:%02x:%02x:%02x:%02x\n",packet[0],packet[1],packet[2],packet[3],packet[4],packet[5]);
+        printf("src ip %u.%u.%u.%u\n",packet[26],packet[27],packet[28],packet[29]);
+        printf("dst ip %u.%u.%u.%u\n",packet[30],packet[31],packet[32],packet[33]);
+        int plen=header->caplen-0x42;
+        if(plen<=0)plen=0;
+        if(plen>10)plen=10;
+        if(plen)
+        {
+            printf("data ");
+            for(int i=0;i<plen;i++)
+            {
+                printf("%02x",packet[0x42+i]);
+            }
+        }
+        printf("\n\n");
 	}
 
 	pcap_close(pcap);
 }
-
